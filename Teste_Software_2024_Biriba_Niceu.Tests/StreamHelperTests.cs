d3f2be9722f0849eae277c523b7f2a105b451463
Niceu Biriba <niceusantos1365@gmail.com>
2024-08-05 07:41:42 -0300

    Arquitetura do Projeto

---
+++ Teste_Software_2024_Biriba_Niceu.Tests/StreamHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using Teste_Software_2024_Biriba_Niceu; // Namespace onde a classe StreamHelper está definida
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace Teste_Software_2024_Biriba_Niceu.Tests
+{
+    [TestClass]
+    public class StreamHelperTests
+    {
+        [TestMethod]
+        public void GenerateStreamFromString_ShouldReturnStreamContainingInputString()
+        {
+            // Arrange
+            string input = "a,b \n c,d, e";
+            var expected = Encoding.UTF8.GetBytes(input);
+
+            // Act
+            using (var stream = StreamHelper.GenerateStreamFromString(input))
+            {
+                // Assert
+                using (var reader = new BinaryReader(stream))
+                {
+                    byte[] result = reader.ReadBytes((int)stream.Length);
+                    CollectionAssert.AreEqual(expected, result);
+                }
+            }
+        }
+    }
+}
